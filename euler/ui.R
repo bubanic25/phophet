02895ea26d4b743c63740cdbcbf3dc86455932d6
bubanic25 <40664827+bubanic25@users.noreply.github.com>
2021-05-14 03:43:40 -0400

    Update ui.R

--- euler/ui.R
+++ euler/ui.R
@@ -1,19 +1,15 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(tidyquant)
 library(forecast)
 library(ggthemes)
+library(forecast)
+library(tidyverse)
 library(tseries)
 library(lubridate)
 library(timetk)
+library(readxl)
+library(tidyquant)
 library(scales)
 library(forecast)  
 library(rlang) #  forecasting pkg
@@ -21,10 +17,12 @@ library(sweep)   # Broom tidiers for forecast pkg
 library(broom)
 library(tibble)
 library(stringr)
+library(highcharter)
 library(knitr)
 library(quantmod)
 library(shinythemes)
 library(ggforce)
+library(prophet)
 
 theme_set(theme_bw(10))
 initialTicker <- "DOJ"
@@ -32,92 +30,101 @@ initialTicker <- "DOJ"
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("slate"),
-                
-                # Application title
+
+    # Application title
+    
+
+    
+   
+    
+    
+    
+    
+    sidebarLayout(
+      sidebarPanel(
+        
+        textInput("ticker", "Ticker", ""),
+        dateInput("dateStart", "Start Date of Analysis:", value = "1900-01-21"),
+        dateInput("dateEnd", "End Date of Analysis:", value = "2021-05-09"),
+       
+        sliderInput("sliderForecast", "Number of Days to Forecast", 180, 1000, 180),
+  
+        
+        
+       
+        
+        helpText("Note: At Default Setting this forecasting simulation takes about 2 minutes to execute per Quote.",
+                 ),
+        
+        actionButton("run", "Run")
+      ),
+      
+      
+      
+      
+      
+      
+      
+   
+    
+    mainPanel(
+      
+      
+      fluidRow(
+        column(           
+                           width = 10,
                
+                 
+                 plotOutput("topPlot", dblclick = "topPlot_dblclick",
+                            brush = brushOpts(
+                              id = "topPlot1_brush",
+                              resetOnNew = TRUE)
                 
-                
-                
-                
-                
-                
-                
-                sidebarLayout(
-                  sidebarPanel(
-                    
-                    textInput("ticker", "Ticker", ""),
-                    dateInput("dateStart", "Start Date of Analysis:", value = "1900-01-21"),
-                    dateInput("dateEnd", "End Date of Analysis:", value = "2021-05-09"),
-                    
-                    sliderInput("sliderForecast", "Number of Days to Forecast", 30, 365, 30),
-                    
-                    
-                    
-                    sliderInput("sliderSims", "Number of Simulations", 100, 10000, 1000),
-                    
-                    helpText("Note: At Default Setting this forecasting simulation takes about 2 minutes to execute per Quote.",
-                             "",
-                          "Increasing the amount of Days to Forecast greatly increases execution times while decreasing predictable probabilities.",
-                             "",
-                             "Increasing the number of simulations preformed greatly increases execution times while increases predictable probabilities"
-                             ),
-                    
-                    actionButton("run", "Run")
-                  ),
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  mainPanel(
-                    
-                    
-                    fluidRow(
-                      column(           
-                        width = 10,
-                        
-                        
-                        plotOutput("topPlot", dblclick = "topPlot_dblclick",
-                                   brush = brushOpts(
-                                     id = "topPlot1_brush",
-                                     resetOnNew = TRUE)
-                                   
-                                   
-                        )
-                      )
-                    ),
-                    
-                    
-                    fluidRow(
-                      column(
-                        
-                        
-                        plotOutput("bottomPlot"),
-                        width = 10
-                        
-                      )
-                      
-                      
-                    )
-                    
+               
                   )
-                  
+        )
+      ),
+      
+      
+      fluidRow(
+        column(
+               
+                 
+                 plotOutput("middlePlot"),
+                 width = 10
+               
                 )
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
+      
+      
+          ),
+      
+      
+      fluidRow(
+        column(
+          
+          
+          plotOutput("bottomPlot"),
+          width = 10
+          
+        )
+        
+        
+      )
+      
+    )
+    
+    )
+      
+      
+      
+      
+    
+    
+ 
+    
+    
+    
+   
+   
+    
 )
